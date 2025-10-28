13fd70d43de1166629bb9a3dbe35ce7ff0d38ab9
Mirko Brombin <brombin94@gmail.com>
2025-10-28 16:39:06 +0100

    Update create-deb.sh

--- create-deb.sh
+++ create-deb.sh
@@ -1,33 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-echo $(nproc)
+echo "$(nproc)"
 
 DIRECTORY=$(pwd)
 export DEBIAN_FRONTEND=noninteractive
 
-LATESTCOMMIT=`cat $DIRECTORY/commit.txt`
+LATESTCOMMIT=$(cat "$DIRECTORY/commit.txt")
 
-function error() {
-	echo -e "\e[91m$1\e[39m"
-  rm -f $COMMITFILE
-  rm -rf $DIRECTORY/box64
-	exit 1
- 	break
+error() {
+  echo -e "\e[91m$1\e[39m"
+  rm -f "$COMMITFILE" 2>/dev/null || true
+  rm -rf "$DIRECTORY/box64" || true
+  exit 1
 }
 
-# Install dependencies
+# Dependencies
 apt-get update
-apt-get install wget git build-essential gcc-11 python3 make gettext pinentry-tty sudo devscripts dpkg-dev cmake checkinstall -y || error "Failed to install dependenceis."
+apt-get install -y \
+  wget git build-essential python3 make gettext \
+  pinentry-tty devscripts dpkg-dev cmake checkinstall \
+  autoconf automake pkg-config ca-certificates jq file binutils || error "Failed to install dependencies."
 
-rm -rf $DIRECTORY/box64
+rm -rf "$DIRECTORY/box64"
 
-cd $DIRECTORY
-
-rm -rf box64
-
+cd "$DIRECTORY"
 git clone https://github.com/ptitSeb/box64 || error "Failed to download box64 repo"
 cd box64
-commit="$(bash -c 'git rev-parse HEAD | cut -c 1-7')"
+commit="$(git rev-parse HEAD | cut -c 1-7)"
+
 if [ "$commit" == "$LATESTCOMMIT" ]; then
   cd "$DIRECTORY"
   rm -rf "box64"
@@ -35,71 +36,66 @@ if [ "$commit" == "$LATESTCOMMIT" ]; then
   touch exited_successfully.txt
   exit 0
 fi
+
 echo "Box64 is not the latest version, compiling now."
-echo $commit > $DIRECTORY/commit.txt
-echo "Wrote commit to commit.txt file for use during the next compilation."
+echo "$commit" > "$DIRECTORY/commit.txt"
 
 targets=(ARM64 SDORYON1 SD888 ANDROID RPI4ARM64 RPI3ARM64 TEGRAX1 RK3399 RK3588 RPI5ARM64 RPI5ARM64PS16K LX2160A TEGRA_T194 M1)
 
-for target in ${targets[@]}; do
+for target in "${targets[@]}"; do
   echo "Building $target"
-
   cd "$DIRECTORY/box64"
-  sudo rm -rf build && mkdir build && cd build || error "Could not move to build directory"
-  # allow installation even on x86_64 (needed for checkinstall)
-  # sed -i "s/NOT _x86 AND NOT _x86_64/true/g" ../CMakeLists.txt
-  # warning, BOX64 cmakelists enables crypto with the ARM_DYNAREC options, it was purly by luck that no crypto opts were used which would be a problem since the Pi4 doesn't have them
+  rm -rf build && mkdir build && cd build || error "Could not move to build directory"
+
   if [[ $target == "ANDROID" ]]; then
-    cmake .. -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-11 -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 || error "Failed to run cmake."
+    cmake .. -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 || error "Failed to run cmake."
   else
-    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-11 -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 || error "Failed to run cmake."
+    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DARM_DYNAREC=ON -DBOX32=1 -DBOX32_BINFMT=1 || error "Failed to run cmake."
   fi
-  make -j8 || error "Failed to run make."
 
-  function get-box64-version() {
-    if [[ $1 == "ver" ]]; then
-      export BOX64VER="$(cat ../src/box64version.h | sed -n -e 's/^.*BOX64_MAJOR //p')"."$(cat ../src/box64version.h | sed -n -e 's/^.*BOX64_MINOR //p')"."$(cat ../src/box64version.h | sed -n -e 's/^.*BOX64_REVISION //p')"
-    elif [[ $1 == "commit" ]]; then
-      export BOX64COMMIT="$commit"
-    fi
-  }
+  make -j"$(nproc)" || error "Failed to run make."
 
-  get-box64-version ver || error "Failed to get box64 version!"
-  get-box64-version commit || error "Failed to get box64 commit!"
-  DEBVER="$(echo "$BOX64VER+$(date +"%F" | sed 's/-//g').$BOX64COMMIT")" || error "Failed to set debver variable."
+  BOX64VER="$(grep -Eo 'BOX64_[A-Z]+ [0-9]+' ../src/box64version.h | awk '{print $2}' | paste -sd '.' -)"
+  BOX64COMMIT="$commit"
+  DEBVER="${BOX64VER}+$(date +'%Y%m%d').${BOX64COMMIT}"
 
   mkdir doc-pak || error "Failed to create doc-pak dir."
-  cp ../README.md ./doc-pak || error "Failed to add readme to docs"
-  cp ../docs/CHANGELOG.md ./doc-pak || error "Failed to add changelog to docs"
-  cp ../docs/USAGE.md ./doc-pak || error "Failed to add USAGE to docs"
-  cp ../LICENSE ./doc-pak || error "Failed to add license to docs"
-  echo "Box64 lets you run x86_64 Linux programs (such as games) on non-x86_64 Linux systems, like ARM (host system needs to be 64bit little-endian)">description-pak || error "Failed to create description-pak."
+  cp ../README.md ./doc-pak/ || true
+  cp ../docs/CHANGELOG.md ./doc-pak/ || true
+  cp ../docs/USAGE.md ./doc-pak/ || true
+  cp ../LICENSE ./doc-pak/ || true
+  echo "Box64 lets you run x86_64 Linux programs (such as games) on non-x86_64 Linux systems, like ARM (host system needs to be 64bit little-endian)" > description-pak
+
   echo "#!/bin/bash
   echo 'Restarting systemd-binfmt...'
-  systemctl restart systemd-binfmt || true" > postinstall-pak || error "Failed to create postinstall-pak!"
+  systemctl restart systemd-binfmt || true" > postinstall-pak
 
   conflict_list="qemu-user-static"
   for value in "${targets[@]}"; do
-    [[ $value != $target ]] && conflict_list+=", box64-$(echo $value | tr '[:upper:]' '[:lower:]' | tr _ - | sed -r 's/ /, /g')"
+    [[ $value != $target ]] && conflict_list+=", box64-$(echo "$value" | tr '[:upper:]' '[:lower:]' | tr _ -)"
   done
-  
+
   if [[ $target == "ARM64" ]]; then
-    sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="arm64" --provides="box64" --conflicts="$conflict_list" --maintainer="Ryan Fortner <ryankfortner@gmail.com>" --pkglicense="MIT" --pkgsource="https://github.com/ptitSeb/box64" --pkggroup="utils" --pkgname="box64" --install="no" make install || error "Checkinstall failed to create a deb package."
+    checkinstall -y -D --pkgversion="$DEBVER" --arch="arm64" \
+      --provides="box64" --conflicts="$conflict_list" \
+      --maintainer="Ryan Fortner <ryankfortner@gmail.com>" \
+      --pkglicense="MIT" --pkgsource="https://github.com/ptitSeb/box64" \
+      --pkggroup="utils" --pkgname="box64" --install="no" make install || error "Checkinstall failed."
   else
-    sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="arm64" --provides="box64" --conflicts="$conflict_list" --maintainer="Ryan Fortner <ryankfortner@gmail.com>" --pkglicense="MIT" --pkgsource="https://github.com/ptitSeb/box64" --pkggroup="utils" --pkgname="box64-$target" --install="no" make install || error "Checkinstall failed to create a deb package."
+    checkinstall -y -D --pkgversion="$DEBVER" --arch="arm64" \
+      --provides="box64" --conflicts="$conflict_list" \
+      --maintainer="Ryan Fortner <ryankfortner@gmail.com>" \
+      --pkglicense="MIT" --pkgsource="https://github.com/ptitSeb/box64" \
+      --pkggroup="utils" --pkgname="box64-$(echo "$target" | tr '[:upper:]' '[:lower:]' | tr _ -)" \
+      --install="no" make install || error "Checkinstall failed."
   fi
 
-  cd $DIRECTORY
-  mv box64/build/*.deb ./debian/ || error "Failed to move deb to debian folder."
-
+  cd "$DIRECTORY"
+  mv box64/build/*.deb ./debian/ || error "Failed to move deb."
 done
 
-# only keep last 2 debs for each target
-# keeps github pages builds fast and below 1GB suggested limit
-# thanks to gman from pi-apps
-cd $DIRECTORY
+cd "$DIRECTORY"
 ls ./debian/box64*.deb | sort -t '+' -k 2 | head -n -24 | xargs -r rm
-
-rm -rf $DIRECTORY/box64
+rm -rf "$DIRECTORY/box64"
 
 echo "Script complete."
